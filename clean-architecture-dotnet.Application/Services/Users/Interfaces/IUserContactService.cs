8d2343ac2e40d941a0b65814dbe4018fb1618d46
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-02-23 22:34:59 -0300

    Adjusted user contact interface

--- clean-architecture-dotnet.Application/Services/Users/Interfaces/IUserContactService.cs
+++ clean-architecture-dotnet.Application/Services/Users/Interfaces/IUserContactService.cs
@@ -1,10 +1,10 @@
-﻿using clean_architecture_dotnet.Application.ViewModels.Users;
-using Microsoft.AspNetCore.Mvc;
+﻿using clean_architecture_dotnet.Application.Models.Http;
+using clean_architecture_dotnet.Application.ViewModels.Users;
 
 namespace clean_architecture_dotnet.Application.Services.Users.Interfaces
 {
     public interface IUserContactService
     {
-        Task<IActionResult> Put(UserContactViewModel address);
+        Task<Result<UserContactViewModel>> Put(UserContactViewModel address);
     }
 }
