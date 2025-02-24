0970a2e4cb0a830079aaee2ae350b54b27896c16
Matheus Ribeiro <omatheusribeiro@outlook.com>
2025-02-24 11:18:11 -0300

    Created mapping class for view models and entities, with this the structure of the Application project was finalized

---
+++ clean-architecture-dotnet.Application/Mappings/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using clean_architecture_dotnet.Application.ViewModels.Products;
+using clean_architecture_dotnet.Application.ViewModels.Sales;
+using clean_architecture_dotnet.Application.ViewModels.Users;
+using clean_architecture_dotnet.Domain.Entities.Products;
+using clean_architecture_dotnet.Domain.Entities.Sales;
+using clean_architecture_dotnet.Domain.Entities.Users;
+
+namespace clean_architecture_dotnet.Application.Mappings
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            #region Users Profile
+
+            CreateMap<User, UserViewModel>();
+            CreateMap<UserAddress, UserAddressViewModel>();
+            CreateMap<UserContact, UserContactViewModel>();
+
+            #endregion
+
+            #region Products Profile
+
+            CreateMap<Product, ProductViewModel>();
+            CreateMap<ProductType, ProductTypeViewModel>();
+
+            #endregion
+
+
+            #region Sales Profile
+
+            CreateMap<Sale, SaleViewModel>();
+
+            #endregion
+        }
+    }
+}
---
+++ clean-architecture-dotnet.Application/Mappings/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using clean_architecture_dotnet.Application.ViewModels.Products;
+using clean_architecture_dotnet.Application.ViewModels.Sales;
+using clean_architecture_dotnet.Application.ViewModels.Users;
+using clean_architecture_dotnet.Domain.Entities.Products;
+using clean_architecture_dotnet.Domain.Entities.Sales;
+using clean_architecture_dotnet.Domain.Entities.Users;
+
+namespace clean_architecture_dotnet.Application.Mappings
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            #region Users Profile
+
+            CreateMap<UserViewModel, User>();
+            CreateMap<UserAddressViewModel, UserAddress>();
+            CreateMap<UserContactViewModel, UserContact>();
+
+            #endregion
+
+            #region Products Profile
+
+            CreateMap<ProductViewModel, Product>();
+            CreateMap<ProductTypeViewModel, ProductType>();
+
+            #endregion
+
+            #region Sales Profile
+
+            CreateMap<SaleViewModel, Sale>();
+
+            #endregion
+        }
+    }
+}
